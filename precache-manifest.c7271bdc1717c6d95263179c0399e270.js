43460506fdc58af6788870e58e2d6e627b1f935c
Balaji <17160351+Balaji2682@users.noreply.github.com>
2020-07-31 02:44:05 +0530

    Updates

---
+++ precache-manifest.c7271bdc1717c6d95263179c0399e270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d875d91cd4327f5c1ecf3911738eafa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a1a2454114b1916a406",
+    "url": "/monsters-rolodex/static/css/main.af62cca5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1a2454114b1916a406",
+    "url": "/monsters-rolodex/static/js/main.cb2d18e2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
